6252cf580fa5d10f7624558d3dd26b1c7d0cba1a
Thomas Abot <boquello-git@yahoo.fr>
2014-12-06 11:53:22 +0100

    Added brownian movement

---
+++ Assets/Scripts/BrownianMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrownianMovement : MonoBehaviour {
+
+	public float intensity = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		float dx = Random.Range(-intensity, intensity);
+		float dz = Random.Range(-intensity, intensity);
+		
+		gameObject.transform.Translate(dx, 0, dz);
+	}
+}
