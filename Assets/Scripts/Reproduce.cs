186a153a47ed73fa26849e8804e38ed4e326e230
Thomas Abot <boquello-git@yahoo.fr>
2014-12-06 22:20:51 +0100

    Added monster reproduction

---
+++ Assets/Scripts/Reproduce.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reproduce : MonoBehaviour {
+
+	public float ratio = 0.1f;
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision collision) {
+		string thisName = collision.gameObject.name;
+		string otherName;
+
+		//TODO : delete "(Clone)" at the end of name if needed.
+	
+		foreach (ContactPoint contact in collision.contacts) {
+			otherName = contact.otherCollider.name;
+			print(thisName + " hit " + otherName);
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+			if( Random.value < ratio && ( thisName.Equals(otherName) || thisName.Equals(otherName + "(Clone)") ) ){
+				GameObject born = (GameObject) Instantiate(Resources.Load("Prefabs/" + name));
+				born.transform.position = new Vector3(this.gameObject.transform.position.x,
+				                                      this.gameObject.transform.position.y,
+				                                      this.gameObject.transform.position.z);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnAdventurer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAdventurer : MonoBehaviour {
+
+	public float periodicity = 1.0f;
+	public float ratio = 0.1f;
+
+	void Start(){
+		InvokeRepeating("Spawn", 0, periodicity);
+	}
+
+	// Update is called once per frame
+	void Spawn () {
+		if(Random.value < ratio){
+			Debug.LogWarning("Let me spawn an adventurer ! Implements me, please.");
+		}
+	}
+}
