ddc24bf1579d1b3815371be8cfca82376c6af882
Thomas Abot <boquello-git@yahoo.fr>
2014-12-06 12:07:59 +0100

    Monster body follow its collider

---
+++ Assets/Scripts/FollowCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCollider : MonoBehaviour {
+
+	private Transform mMovement;
+
+	void Start () {
+		mMovement =  gameObject.transform.Find("Movement");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position  = new Vector3(mMovement.position.x, 0, mMovement.position.y);
+	}
+}
