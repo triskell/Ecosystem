d766a86bccdcd236828a45fc9bec080a44a1b1ae
Thomas Abot <boquello-git@yahoo.fr>
2014-12-07 12:25:12 +0100

    Fixed renaming new instances of Monsters as clones

--- Assets/Scripts/Reproduce.cs
+++ Assets/Scripts/Reproduce.cs
@@ -8,18 +8,14 @@ public class Reproduce : MonoBehaviour {
 	// Update is called once per frame
 	void OnCollisionEnter (Collision collision) {
 		string thisName, otherName;
-
-		//TODO : delete "(Clone)" at the end of name if needed.
 	
 		foreach (ContactPoint contact in collision.contacts) {
 			thisName = contact.thisCollider.name;
 			otherName = contact.otherCollider.name;
 
-			Debug.Log(thisName + " hit " + otherName);
-			Debug.DrawRay(contact.point, contact.normal, Color.white);
-
 			if( Random.value < ratio && ( thisName.Equals(otherName) || thisName.Equals(otherName + "(Clone)") ) ){
 				GameObject born = (GameObject) Instantiate(Resources.Load("Prefabs/" + name));
+				born.name = thisName;
 				born.transform.position = new Vector3(this.gameObject.transform.position.x,
 				                                      this.gameObject.transform.position.y,
 				                                      this.gameObject.transform.position.z);
