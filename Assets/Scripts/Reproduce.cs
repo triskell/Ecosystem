6fe2ce501ffb930bed3629736084a535944c4fca
Thomas Abot <boquello-git@yahoo.fr>
2014-12-08 13:05:34 +0100

    Preventing monsters from reproducing too quickly

--- Assets/Scripts/Reproduce.cs
+++ Assets/Scripts/Reproduce.cs
@@ -4,7 +4,15 @@ using System.Collections;
 public class Reproduce : MonoBehaviour {
 
 	public float ratio = 0.1f;
-	
+	public int JAUGE_MAX = 100;
+	private int jauge = 0;
+
+	void Update () {
+		if(jauge < JAUGE_MAX){
+			jauge++;
+		}
+	}
+
 	// Update is called once per frame
 	void OnCollisionEnter (Collision collision) {
 		string thisName, otherName;
@@ -13,12 +21,13 @@ public class Reproduce : MonoBehaviour {
 			thisName = contact.thisCollider.name;
 			otherName = contact.otherCollider.name;
 
-			if( Random.value < ratio && ( thisName.Equals(otherName) || thisName.Equals(otherName + "(Clone)") ) ){
+			if( Random.value < ratio && jauge == JAUGE_MAX && ( thisName.Equals(otherName) || thisName.Equals(otherName + "(Clone)") ) ){
 				GameObject born = (GameObject) Instantiate(Resources.Load("Prefabs/" + name));
 				born.name = thisName;
 				born.transform.position = new Vector3(this.gameObject.transform.position.x,
 				                                      this.gameObject.transform.position.y,
 				                                      this.gameObject.transform.position.z);
+				jauge = 0;
 			}
 		}
 	}
